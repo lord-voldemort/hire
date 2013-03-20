dff9788c0b816d30fbabadc33ba8439cedbe1480
Alex Gorski <alex.gorski@gmail.com>
2013-03-20 14:55:42 -0400

    Students w delete function

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,12 +16,12 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
   def edit
     @student = Student.find(params[:id])
   end
   
   def update
-   
     @student = Student.find(params[:id])
     if @student.update_attributes(params[:student])
       redirect_to action: :show, id: @student.id
@@ -31,7 +31,9 @@ class StudentsController < ApplicationController
   end
   
   def destroy
-
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to root_path
   end
 
 
