36c45469fba4e9b65d204c09273324c58d1a55ae
Alex Gorski <alex.gorski@gmail.com>
2013-03-20 16:15:22 -0400

    built employers

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,18 +1,39 @@
 class EmployersController < ApplicationController
   def new
-
+    @employer = Employer.new
   end
 
   def create
-
+    @employer = Employer.new(params[:employer])
+    @employer.save
+    redirect_to employers_path
   end
 
   def index
     @employers = Employer.all
   end
+
   def show
-    @students = Student.all
     @employer = Employer.find(params[:id])
   end
 
+  def edit
+    @employer = Employer.find(params[:id])
+  end
+  
+  def update
+    @employer = Employer.find(params[:id])
+    if @employer.update_attributes(params[:employer])
+      redirect_to action: :show, id: @employer.id
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @employer = Employer.find(params[:id])
+    @employer.destroy
+    redirect_to employers_path
+  end
+  
 end
--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,5 +1,5 @@
 class Employer < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :industry, :location, :est_year
   has_many :interviews
   has_many :students, :through => :interviews
 end
