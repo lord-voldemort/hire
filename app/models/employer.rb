e4c5374708456641032e674b584663d5c10bd581
Alex Gorski <alex.gorski@gmail.com>
2013-03-20 17:28:39 -0400

    superficial checkbox added to Employers list, lines 10-12 of Employers view

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -2,7 +2,7 @@ class Employer < ActiveRecord::Base
   attr_accessible :name, :industry, :location, :est_year, :students_attributes
   has_many :preferences
   has_many :students, :through => :preferences
-  accepts_nested_attributes_for :students
+
 
   has_many :interviews
   has_many :students, :through => :interviews
