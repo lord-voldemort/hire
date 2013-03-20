ba9dd46811f294090c0a9277be8b86a011e04460
Alex Gorski <alex.gorski@gmail.com>
2013-03-19 21:15:01 -0400

    added index views

---
+++ app/controllers/employers_controller.rb
@@ -0,0 +1,17 @@
+class EmployersController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+    @employers = Employer.all
+  end
+  def show
+
+  end
+
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def index
+    @students = Student.all
+  end
+  def show
+
+  end
+
+
+end
