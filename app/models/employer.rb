817130b704c45aa494e7437363b956a213d1795e
Alex Gorski <alex.gorski@gmail.com>
2013-03-21 16:06:39 -0400

    Add interests to Student...yes, our branch ended up with the wrong name

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,9 +1,11 @@
 class Employer < ActiveRecord::Base
   attr_accessible :name, :industry, :location, :est_year, :students_attributes
+  
+  has_many :interviews
+  has_many :students, :through => :interviews
+
   has_many :preferences
   has_many :students, :through => :preferences, :dependent => :destroy
 
-  has_many :interviews
-  has_many :students, :through => :interviews
   accepts_nested_attributes_for :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ActiveRecord::Base
   attr_accessible :name, :preferred_industry, :preferred_location, :employers_attributes
+  
   has_many :interviews
   has_many :employers, :through => :interviews
 
   has_many :preferences
   has_many :employers, :through => :preferences, :dependent => :destroy
-
 
   accepts_nested_attributes_for :employers 
 end
