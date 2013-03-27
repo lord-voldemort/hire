bde7bd1197c60047f0c687b9230edfea1d569ac9
Alex Gorski <alex.gorski@gmail.com>
2013-03-27 16:12:16 -0400

    interview validations and update to interview show view

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,8 +1,13 @@
 class Interview < ActiveRecord::Base
-  attr_accessible :student_id, :employer_id, :interview_date, :interview_location, 
-  :status, :follow_up, :interview_appts_attributes
+  attr_accessible :student_id, :employer_id, :status, :follow_up, :interview_appts_attributes
   belongs_to :student
   belongs_to :employer
   has_many  :interview_appts
   accepts_nested_attributes_for :interview_appts
+
+  
+  validates :student_id, :presence => true
+  validates :employer_id, :presence => true
+  validates :status, :presence => true
+  validates_uniqueness_of :student_id, :scope => [:employer_id], message: "Interview already exists"
 end
