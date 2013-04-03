f065faa025650b76363c021fb98558764267f91d
WallisWT <rubys@intertwingly.net>
2013-04-03 13:42:39 -0400

    everything working but for new & unsaved remove button

--- app/assets/javascripts/interviews.js
+++ app/assets/javascripts/interviews.js
@@ -1,26 +1,27 @@
 jQuery(function() {
   $('form').on('click', '.remove_appts', function(event) {
-  $(this).prev('input[type=hidden]').val('1');
-  $(this).closest('fieldset').hide();
-  return event.preventDefault();
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').hide();
+    return event.preventDefault();
   });
 });
 
 jQuery(function() {
   $('.add_appts').click(function(event) {  
-  //debugger
-  $new_fieldset = $('.interview_appt:first').clone();
-  $new_fieldset.find("input").val("");
-  new_fieldset = $new_fieldset.html()
-  size = $('.interview_appt').length;
-  size_less_1 = size - 1
-  var regexp = new RegExp('_' + size_less_1 + '_', 'g');
-  var new_id = '_' + size + '_';
-  update_1 = new_fieldset.replace(regexp, new_id);
-  var regexp2 = new RegExp('\\[' + size_less_1 + ']', 'g');
-  var new_name = '[' + size + ']';
-  update_2 = update_1.replace(regexp2, new_name);
-  $('.interview_appt').append(update_2);
+    $new_fieldset = $('.interview_appt:first').clone();
+    $new_fieldset.find("input").attr("value", "");
+    new_fieldset = $new_fieldset.html()
+    size = $('.interview_appt').length;
+    var regexp = new RegExp('_0_', 'g');
+    var new_id = '_' + size + '_';
+    update_1 = new_fieldset.replace(regexp, new_id);
+    var regexp2 = new RegExp('\\[0]', 'g');
+    var new_name = '[' + size + ']';
+    update_2 = update_1.replace(regexp2, new_name);
+    console.log(update_2)
+    $('.interview_appt:last').after(update_2);
+    event.preventDefault();
+    return false;
   });
 });
 
