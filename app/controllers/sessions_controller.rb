87aba6a7766b90963b2e56262c8676cfc755dfb3
Alex Gorski <alex.gorski@gmail.com>
2013-04-08 13:44:42 -0400

    downcase the employer emails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
+    binding.pry
     if user.role == "Student"
       student = Student.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
