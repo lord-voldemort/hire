5fe68b333f07bafb77aa69584fd642ce30bcaedc
WallisWT <rubys@intertwingly.net>
2013-04-11 14:41:13 -0400

    fix: selecting correct Employer for Preference by looking based on email not ID

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,20 +1,21 @@
 class PreferencesController < ApplicationController
 
   def new
+
     @preference = Preference.new
     if current_user.role == "Student"
       @preference.student_id = Student.where(:user_id => current_user.id).first.id 
       @preference.interest_expressed_by = "Student"
     elsif current_user.role == "Employer"
-      @preference.employer_id = Employer.where(:user_id => current_user.id).first.id
+      #binding.pry
+      #would need to adjust next line if Employers have more than 1 
+      @preference.employer_id = Employer.where(:email => current_user.email).first.id
       @preference.interest_expressed_by = "Employer"
       @preference.student_id = params[:format]
-     
     else
   
     end
 
-    #if @student
   end
 
   def create
