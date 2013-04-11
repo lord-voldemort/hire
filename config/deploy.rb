bea940e09cdffb2ba30e310dfda2ac19fa6ff712
aviflombaum <avi@flombaum.com>
2013-04-11 15:47:20 -0400

    capified and deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,46 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano' # for bundler support
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :application, "hire-app"
+set :repository,  "git@github.com:flatiron-school/hire.git"
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+default_run_options[:pty] = true
+
+set :user, 'deploy'
+set :deploy_to, "/home/#{user}/#{application}"
+set :use_sudo, false
+
+default_run_options[:pty] = true
+
+role :web, "198.211.116.12"                          # Your HTTP server, Apache/etc
+role :app, "198.211.116.12"                          # This may be the same as your `Web` server
+role :db,  "198.211.116.12", :primary => true # This is where Rails migrations will run
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
-
+before "deploy:assets:precompile", "deploy:symlink_configs"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :symlink_configs, :roles => :app do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config"
+    # run "ln -nfs #{shared_path}/twitter.yml #{release_path}/config"
+    # run "ln -nfs #{shared_path}/api_keys.yml #{release_path}/config"
+  end
+end
+
+task :upload_confs, :roles => :app do
+  # upload('config/twitter.yml', "#{shared_path}/twitter.yml")
+  upload('config/database.yml', "#{shared_path}/database.yml")
+end
